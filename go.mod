caed024702d141d046201cf627aced9b1c849de8
bighuangbee <1191352688@qq.com>
2023-03-04 00:08:38 +0800

    rpc client

--- go.mod
+++ go.mod
@@ -14,9 +14,12 @@ require (
 	github.com/nicksnyder/go-i18n/v2 v2.2.1
 	github.com/pkg/errors v0.9.1
 	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
+	github.com/shimingyah/pool v1.0.0
+	github.com/shopspring/decimal v1.3.1
 	github.com/sirupsen/logrus v1.9.0
 	go.uber.org/zap v1.23.0
 	golang.org/x/text v0.4.0
+	google.golang.org/grpc v1.46.2
 	google.golang.org/protobuf v1.28.1
 	gorm.io/driver/mysql v1.4.7
 	gorm.io/gorm v1.24.1
@@ -45,9 +48,9 @@ require (
 	go.opentelemetry.io/otel/trace v1.7.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	google.golang.org/genproto v0.0.0-20220519153652-3a47de7e79bd // indirect
-	google.golang.org/grpc v1.46.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ tools/grpc_client.go
@@ -0,0 +1,47 @@
+package tools
+
+import (
+	"log"
+	"sync"
+	"time"
+
+	"github.com/shimingyah/pool"
+	"google.golang.org/grpc"
+)
+
+var (
+	// 初始化一次，key=grpc addr
+	grpcOnceMap sync.Map
+	// 复用连接，key=grpc addr
+	poolConnMap sync.Map
+)
+
+// 支持复用连接、重连
+// grpc服务地址或者 服务注册中心的地址
+func GetGrpcClient(address string) *grpc.ClientConn {
+
+	actual, isOld := grpcOnceMap.LoadOrStore(address, sync.Once{})
+	if !isOld {
+		grpcOnce := actual.(sync.Once)
+		grpcOnce.Do(func() {
+			poolConn, err := pool.New(address, pool.DefaultOptions)
+			if err != nil {
+				log.Fatalf("failed to new pool: %v", err)
+			}
+			poolConnMap.Store(address, poolConn)
+		})
+	}
+	var poolConnInMap interface{}
+	var ok bool
+	poolConnInMap, ok = poolConnMap.Load(address)
+	for !ok {
+		time.Sleep(time.Millisecond * 100)
+		poolConnInMap, ok = poolConnMap.Load(address)
+	}
+	poolConn := poolConnInMap.(pool.Pool)
+	conn, err := poolConn.Get()
+	if err != nil {
+		log.Fatalf("failed to get conn: %v", err)
+	}
+	return conn.Value()
+}
