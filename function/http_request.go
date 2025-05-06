d7a4d21a8593d99070f898c4d11ffdd795ab94a7
大黄蜂 <1191352688@qq.com>
2025-05-06 17:08:41 +0800

    form post

--- function/http_request.go
+++ function/http_request.go
@@ -3,10 +3,12 @@ package function
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"mime/multipart"
 	"net/http"
+	"net/url"
 	"os"
 	"time"
 )
@@ -92,4 +94,66 @@ func HttpFormPost(addr string, params map[string]string, formfile *FormFile) ([]
 	defer resp.Body.Close()
 
 	return io.ReadAll(resp.Body)
+}
+
+func RequestForm(method, urlStr string, fields map[string]interface{}, header map[string]interface{}) ([]byte, error) {
+	var req *http.Request
+	var err error
+
+	if method == "GET" {
+		// 构造查询参数
+		qs := url.Values{}
+		for key, value := range fields {
+			qs.Add(key, fmt.Sprintf("%v", value))
+		}
+		urlStr = urlStr + "?" + qs.Encode()
+		req, err = http.NewRequest("GET", urlStr, nil)
+		if err != nil {
+			return nil, err
+		}
+	} else if method == "POST" {
+		// 创建 multipart/form-data 请求体
+		body := &bytes.Buffer{}
+		writer := multipart.NewWriter(body)
+
+		for key, value := range fields {
+			err := writer.WriteField(key, fmt.Sprintf("%v", value))
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		// 关闭 writer 以结束 multipart 数据
+		err = writer.Close()
+		if err != nil {
+			return nil, err
+		}
+
+		req, err = http.NewRequest("POST", urlStr, body)
+		if err != nil {
+			return nil, err
+		}
+
+		// 使用 writer 自动生成的 Content-Type
+		req.Header.Set("Content-Type", writer.FormDataContentType())
+	} else {
+		return nil, errors.New("unsupported HTTP method")
+	}
+
+	// 添加请求头
+	for key, value := range header {
+		req.Header.Add(key, fmt.Sprintf("%v", value))
+	}
+
+	// 发起 HTTP 请求
+	client := &http.Client{}
+	response, err := client.Do(req)
+	defer response.Body.Close()
+
+	respBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, errors.New("请求出错, " + err.Error())
+	}
+
+	return respBody, nil
 }
